6097839fa67359ae8f95579f0e1f17eb9e1c780f
Angellsworth <angellsworth@msn.com>
2025-04-04 20:23:32 -0600

    wip: added user model

--- models/user.js
+++ models/user.js
@@ -1,56 +1,65 @@
-// models/user.js - w/quiz results
+// models/user.js
+//User model with quiz results now reference instead of embedded
+
+
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  hashedPassword: {
-    type: String,
-    required: true,
-  },
-  profileImg: {
-    type: String,
-    default: '',
-  },
-  pinnedOutfits: [{
-    type: mongoose.Schema.Types.ObjectId,// use .populate() to get outfit details
-    ref: 'Outfit',
-  }],
-  followers: [{
-    type: mongoose.Schema.Types.ObjectId,// use .populate() to get follower details
-    ref: 'User',
-  }],
 
-  following: [{
-    type: mongoose.Schema.Types.ObjectId,// use .populate() to get following details
-    ref: 'User',
-  }],
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  //embedded quiz results
-  genderCategory: String,
-  styleProfile: String,
-  lifestyleTags: [String],
-  climateRegion: String,
-  seasonalPrefs: [String],
-  fitPreference: String,
-  quizTakenAt: Date
+const userSchema = new mongoose.Schema({
+ username: {
+   type: String,
+   required: true,
+ },
+ email: {
+   type: String,
+   required: true,
+ },
+ hashedPassword: {
+   type: String,
+   required: true,
+   //TODO: Install BCRYPT to hash the passwords?
+ },
+ profileImg: {
+   type: String,
+   default: '',
+ },
+ pinnedOutfits: [{
+   type: mongoose.Schema.Types.ObjectId,// User.findById(userId).populate('pinnedOutfits')
+   ref: 'Outfit',
+ }],
+ followers: [{
+   type: mongoose.Schema.Types.ObjectId,// User.findById(userId).populate('followers')
+   ref: 'User',
+    //TODO: Follower Count?
+    //TODO: How to handle duplicate followers?
+ }],
+
+
+ following: [{
+   type: mongoose.Schema.Types.ObjectId,// const user = await User.findById(userId).populate('following');
+   ref: 'User',
+   //TODO: Following Count?
+ }],
+ createdAt: {
+   type: Date,
+   default: Date.now,
+ },
+ quizResults: {
+   type: mongoose.Schema.Types.ObjectId,// User.findById(userId).populate('quizResults')
+   ref: 'QuizResults'
+   //TODO: Can the user take multiple quizzes? or just one
+ }
+
 
 });
+
 
 userSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    delete returnedObject.hashedPassword;
-    return returnedObject
-  }
+ transform: (document, returnedObject) => {
+   delete returnedObject.hashedPassword;
+   return returnedObject
+ }
 });
+
 
 module.exports = mongoose.model('User', userSchema);
