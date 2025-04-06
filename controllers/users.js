c153506cda1172209772970fa8e1d0d95ae04053
RConradGe <rcgadduang@gmail.com>
2025-04-06 10:33:38 -0700

    wip: added populate methods for followers, following, and pinnedOutfits

--- controllers/users.js
+++ controllers/users.js
@@ -21,7 +21,10 @@ router.get('/:userId', verifyToken, async (req, res) => {
       return res.status(403).json({ err: "Unauthorized"});
     }
 
-    const user = await User.findById(req.params.userId);
+    const user = await User.findById(req.params.userId)
+      .populate('followers', 'username')
+      .populate('following', 'username')
+      .populate('pinnedOutfits');
 
     if (!user) {
       return res.status(404).json({ err: 'User not found.'});
