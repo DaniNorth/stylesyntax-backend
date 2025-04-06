3f0dda66c465b0bf904b38284a8cd4263cca052c
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-04-05 20:32:58 -0700

    wip: added is admin condition to allow extra security for certain protected actions

--- models/user.js
+++ models/user.js
@@ -6,53 +6,55 @@ const mongoose = require('mongoose');
 
 
 const userSchema = new mongoose.Schema({
- username: {
-   type: String,
-   required: true,
- },
- email: {
-   type: String,
-   required: true,
- },
- hashedPassword: {
-   type: String,
-   required: true,
-   //TODO: Install BCRYPT to hash the passwords?
- },
- profileImg: {
-   type: String,
-   default: '',
- },
- pinnedOutfits: [{
-   type: mongoose.Schema.Types.ObjectId,// User.findById(userId).populate('pinnedOutfits')
-   ref: 'Outfit',
- }],
- followers: [{
-   type: mongoose.Schema.Types.ObjectId,// User.findById(userId).populate('followers')
-   ref: 'User',
-    //TODO: Follower Count?
-    //TODO: How to handle duplicate followers?
- }],
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  hashedPassword: {
+    type: String,
+    required: true,
+    //TODO: Install BCRYPT to hash the passwords?
+  },
+  profileImg: {
+    type: String,
+    default: '',
+  },
+  pinnedOutfits: [{
+    type: mongoose.Schema.Types.ObjectId,// User.findById(userId).populate('pinnedOutfits')
+    ref: 'Outfit',
+  }],
+  followers: [{
+    type: mongoose.Schema.Types.ObjectId,// User.findById(userId).populate('followers')
+    ref: 'User',
+      //TODO: Follower Count?
+      //TODO: How to handle duplicate followers?
+  }],
 
 
- following: [{
-   type: mongoose.Schema.Types.ObjectId,// const user = await User.findById(userId).populate('following');
-   ref: 'User',
-   //TODO: Following Count?
- }],
- createdAt: {
-   type: Date,
-   default: Date.now,
- },
- quizResults: {
-   type: mongoose.Schema.Types.ObjectId,// User.findById(userId).populate('quizResults')
-   ref: 'QuizResults'
-   //TODO: Can the user take multiple quizzes? or just one
- }
+  following: [{
+    type: mongoose.Schema.Types.ObjectId,// const user = await User.findById(userId).populate('following');
+    ref: 'User',
+    //TODO: Following Count?
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  quizResults: {
+    type: mongoose.Schema.Types.ObjectId,// User.findById(userId).populate('quizResults')
+    ref: 'QuizResults'
+    //TODO: Can the user take multiple quizzes? or just one
+  },
 
-
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
 });
-
 
 userSchema.set('toJSON', {
  transform: (document, returnedObject) => {
