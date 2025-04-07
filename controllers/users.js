b028838729bb09beac9177b3eca31454aef00b25
RConradGe <rcgadduang@gmail.com>
2025-04-07 00:25:06 -0700

    wip: added a route so user can upload profile photos

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,8 @@ const User = require('../models/user');
 
 const verifyToken = require('../middleware/verify-token');
 
+const upload = require('../middleware/upload');
+
 router.get('/', verifyToken, async (req, res) => {
   try {
     const users = await User.find({}, "username");
@@ -36,6 +38,24 @@ router.get('/:userId', verifyToken, async (req, res) => {
 
     res.json({ user });
   } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+//  Route so user can upload photos
+router.post('/:userId/upload-profile-pic', verifyToken, upload.single('image'), async (req, res) => {
+  try {
+    const imageUrl = req.file.path;  
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      { profileImg: imageUrl }, 
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    console.error(err);
     res.status(500).json({ err: err.message });
   }
 });
