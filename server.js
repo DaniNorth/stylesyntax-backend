ca52c7a95263a567463cfa351215f1cb388ca530
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-04-10 16:41:48 -0700

    wip: added change for deployment to netlify

--- server.js
+++ server.js
@@ -23,7 +23,10 @@ mongoose.connection.on('connected', () => {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://your-site-name.netlify.app',
+  credentials: true, 
+}));
 app.use(express.json());
 app.use(logger('dev'));
 
